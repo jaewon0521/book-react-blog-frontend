5c4fb2c60148164d09e303a22612edf1947030b3
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-05-12 15:33:48 +0900

    login ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "../node_modules/react-router-dom/index";
+import LoginPage from "./pages/LoginPage";
+import PostListPage from "./pages/PostListPage";
+import PostPage from "./pages/PostPage";
+import RegisterPage from "./pages/RegisterPage";
+import WritePage from "./pages/WritePage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<PostListPage />} />
+        <Route path='/@:userName' element={<PostListPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/@:userName/:postId" element={<PostPage />} />
+      </Routes>
+    </>
   );
 }
 
