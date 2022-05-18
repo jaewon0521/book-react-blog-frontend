3df81e19715885b8da78fdb795be0f66cceb24e8
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-05-18 22:34:11 +0900

    redux-saga login and register middleware

--- src/modules/auths.js
+++ src/modules/auths.js
@@ -1,8 +1,14 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import createRequestSaga, { createRequestActionTypes } from '../lib/createRequestSaga';
+import { takeLatest } from 'redux-saga/effects';
+import * as authAPI from '../lib/api/auth';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes('auth/REGISTER');
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes('auth/LOGIN');
 
 export const changeField = createAction(
   CHANGE_FIELD,
@@ -15,6 +21,25 @@ export const changeField = createAction(
 
 export const initializeForm = createAction(INITIALIZE_FORM, form => form); //register login
 
+export const register = createAction(REGISTER, ({ userName, password }) => ({
+  userName,
+  password
+}))
+
+export const login = createAction(LOGIN, ({ userName, password }) => ({
+  userName,
+  password
+}));
+
+// 사가 생성
+const registerSaga = createRequestSaga(REGISTER, authAPI.register);
+const loginSaga = createRequestSaga(LOGIN, authAPI.login);
+export function* authSaga() {
+  yield takeLatest(REGISTER, registerSaga);
+  yield takeLatest(LOGIN, loginSaga);
+}
+
+
 const initialState = {
   register: {
     userName: '',
@@ -25,6 +50,8 @@ const initialState = {
     userName: '',
     password: '',
   },
+  auth: null,
+  authError: null,
 }
 
 
@@ -37,7 +64,26 @@ const auth = handleActions(
     [INITIALIZE_FORM]: (state, { payload: form }) => ({
       ...state,
       [form]: initialState[form],
-    })
+      authError: null, // 폼 전환 시 회원 인증 에러 초기화
+    }),
+    [REGISTER_SUCCESS]: (state, { payload: auth }) => ({
+      ...state,
+      authError: null,
+      auth,
+    }),
+    [REGISTER_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      authError: error
+    }),
+    [LOGIN_SUCCESS]: (state, { payload: auth }) => ({
+      ...state,
+      authError: null,
+      auth
+    }),
+    [LOGIN_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      authError: error
+    }),
   },
   initialState,
 );
--- src/modules/loading.js
+++ src/modules/loading.js
@@ -1,4 +1,32 @@
-import { createAction } from "redux-actions";
+import { createAction, handleActions } from "redux-actions";
 
 const START_LOADING = 'loading/START_LOADING';
 const FINISH_LOADING = 'loading/FINISH_LOADING';
+
+export const startLoading = createAction(
+  START_LOADING,
+  requestType => requestType
+);
+
+export const finishLoading = createAction(
+  FINISH_LOADING,
+  requestType => requestType
+)
+
+const initailState = {};
+
+const loading = handleActions(
+  {
+    [START_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: true,
+    }),
+    [FINISH_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: false,
+    })
+  },
+  initailState,
+);
+
+export default loading;
