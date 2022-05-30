27098f87eae349c19aea8ffc270e6ead25d22431
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-05-30 13:28:06 +0900

    write post action saga

---
+++ src/containers/write/WriteActionButtonsContainer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import WriteActionButtons from '../../components/write/WriteActionButtons';
+import { writePost } from '../../modules/write';
+
+const WriteActionButtonsContainer = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { title, body, tags, post, postError } = useSelector(({ write }) => ({
+    title: write.title,
+    body: write.body,
+    tags: write.tags,
+    post: write.post,
+    postError: write.postError,
+  }));
+
+  //포스트 등록
+  const onPublish = () => {
+    dispatch(writePost({
+      title,
+      body,
+      tags
+    }),
+    );
+  }
+
+  //취소
+  const onCancel = () => {
+    navigate(-1);
+  }
+
+  useEffect(() => {
+    if (post) {
+      const { _id, user } = post;
+      navigate(`/@${user.userName}/${_id}`);
+    }
+    if (postError) {
+      console.log(postError);
+    }
+  }, [navigate, post, postError]);
+  return <WriteActionButtons onPublish={onPublish} onCancel={onCancel} />;
+};
+
+export default WriteActionButtonsContainer;
--- src/modules/write.js
+++ src/modules/write.js
@@ -1,18 +1,35 @@
 import { createAction, handleActions } from "redux-actions";
+import createRequestSaga, { createRequestActionTypes } from "../lib/createRequestSaga";
+import * as postsAPI from '../lib/api/posts';
+import { takeLatest } from 'redux-saga/effects';
 
 const INITIALIZE = 'write/INITAILIEZ'; //모든 내용 초기화
 const CHANGE_FIELD = 'wriete/CHANGE_FIELD'; // 특정 key 값 바꾸기
+const [WRITE_POST, WRITE_POST_SUCCESS, WRITE_POST_FAILURE] = createRequestActionTypes('write/WRITE_POST'); // 포스트 작성
 
 export const initailize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
   key,
   value
 }));
+export const writePost = createAction(WRITE_POST, ({ title, body, tags }) => ({
+  title,
+  body,
+  tags
+}));
+
+//사가 생성
+const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
+export function* writeSaga() {
+  yield takeLatest(WRITE_POST, writePostSaga);
+}
 
 const initalState = {
   title: '',
   body: '',
-  tags: []
+  tags: [],
+  post: null,
+  postError : null,
 };
 
 const write = handleActions(
@@ -22,6 +39,19 @@ const write = handleActions(
       ...state,
       [key]: value, // 특정 key 값을 업데이트
     }),
+    [WRITE_POST]: state => ({
+      ...state,
+      post: null,
+      postError: null
+    }),
+    [WRITE_POST_SUCCESS]: (state, { payload: post }) => ({
+      ...state,
+      post
+    }),
+    [WRITE_POST_FAILURE]: (state, { payload: postError }) => ({
+      ...state,
+      postError,
+    })
   },
   initalState
 );
