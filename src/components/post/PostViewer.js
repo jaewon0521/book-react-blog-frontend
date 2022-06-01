3be344ede7c3e659e8f0d98a9ced2b3c6e09d37a
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-06-01 21:59:16 +0900

    postViewr Page UI

---
+++ src/components/post/PostViewer.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette'
+import Responsive from '../common/Responsive';
+
+const PostViewerBlock = styled(Responsive)`
+  margin-top: 4rem;
+`
